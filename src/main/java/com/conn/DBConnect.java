ee216fbfe9edcc58b36f344f7360b4844dac55f4
itsurvey6-lab <itsurvey6@gmail.com>
2026-08-18 09:50:30 +0300

    Fix SQLite database path

--- src/main/java/com/conn/DBConnect.java
+++ src/main/java/com/conn/DBConnect.java
@@ -14,7 +14,7 @@ public class DBConnect
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
-			conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
+			conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
 			
 		}catch (Exception e) {
 			e.printStackTrace();
